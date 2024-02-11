3037551cbea91c41a54eddae61b624c02615236e
Luis Felipe <luis.felipe.lbs@gmail.com>
2024-02-11 13:35:57 -0300

    removed cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -30,22 +30,6 @@ import { deliverOrder } from './routes/deliver-order'
 import { getFriendlyFace } from './routes/ friendly-face'
 
 const app = new Elysia()
-  .use(
-    cors({
-      credentials: true,
-      allowedHeaders: ['content-type'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-      origin: (request): boolean => {
-        const origin = request.headers.get('origin')
-
-        if (!origin) {
-          return false
-        }
-
-        return true
-      },
-    }),
-  )
   .use(swagger())
   .use(getFriendlyFace)
   .use(authentication)
